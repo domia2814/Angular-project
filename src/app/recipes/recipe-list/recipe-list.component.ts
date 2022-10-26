3c0e80adc958fe8e097293a4b80a4124600b5e4b
domia2814 <106426622+domia2814@users.noreply.github.com>
2022-10-26 11:41:47 +0200

    inicialization

---
+++ src/app/recipes/recipe-list/recipe-list.component.ts
@@ -0,0 +1,20 @@
+import { Component, OnInit } from '@angular/core';
+import { Recipe } from '../recipe.model';
+
+@Component({
+  selector: 'app-recipe-list',
+  templateUrl: './recipe-list.component.html',
+  styleUrls: ['./recipe-list.component.css']
+})
+export class RecipeListComponent implements OnInit {
+  recipes: Recipe[] = [   // to jest nasz własny "przepis", własny typ, został okreslony w recipe.model.ts
+  new Recipe('A test recipe', 'this is a simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272'), 
+  new Recipe('A test recipe', 'this is a simply test', 'https://images.immediate.co.uk/production/volatile/sites/30/2020/08/chorizo-mozarella-gnocchi-bake-cropped-9ab73a3.jpg?quality=90&webp=true&resize=300,272')
+  ];
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
